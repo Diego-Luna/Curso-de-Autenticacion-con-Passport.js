853673749d2c2219ba149247c74078954cffbf29
Diego Luna <diegofcolunalopez@gmail.com>
2020-07-29 10:33:10 -0500

    Comunicacion maquina a maquina

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const passport = require('passport');
+const boom = require('@hapi/boom');
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
 
 const { config } = require("./config");
 
@@ -6,27 +10,79 @@ const app = express();
 
 // body parser
 app.use(express.json());
+app.use(cookieParser());
 
-app.post("/auth/sign-in", async function(req, res, next) {
+//  Basic strategy
+require("./utils/auth/strategies/basic");
+
+app.post("/auth/sign-in", async function (req, res, next) {
+  passport.authenticate("basic", function (error, data) {
+    try {
+      // si hay un error o no hay datos
+      if (error || !data) {
+        // regresamos un error
+        next(boom.unauthorized());
+      }
+
+      // si toda sali bien
+      // la session va a hacer falso, por que no manejamos estado
+      req.login(data, { session: false }, async function (error) {
+        // si hay un error, llamamos a next
+        if (error) {
+          next(error);
+        }
+
+        // destructuramos nuestros datos
+        const { token, ...user } = data;
+
+        // definimos una cookie,
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev
+        });
+        // en produccion lo devemos acseder por http, por un servidor
+        // y sea manajada por HTTPS
+
+        // regresamos un ok, y el usuarios
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post("/auth/sign-up", async function (req, res, next) {
+  // traemos el user de body
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: 'POST',
+      data: user
+    });
+
+    // es el codigo http de que fue creado
+    res.status(201).json({ message: 'user created' });
+  } catch (error) {
+    next(error);
+  }
 
 });
 
-app.post("/auth/sign-up", async function(req, res, next) {
+app.get("/movies", async function (req, res, next) {
 
 });
 
-app.get("/movies", async function(req, res, next) {
+app.post("/user-movies", async function (req, res, next) {
 
 });
 
-app.post("/user-movies", async function(req, res, next) {
+app.delete("/user-movies/:userMovieId", async function (req, res, next) {
 
 });
 
-app.delete("/user-movies/:userMovieId", async function(req, res, next) {
-
-});
-
-app.listen(config.port, function() {
+app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
 });
---
+++ ssr-server/utils/auth/strategies/basic.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const { BasicStrategy } = require('passport-http');
+const boom = require('@hapi/boom');
+const axios = require('axios');
+const { config } = require("../../../config/index");
+
+// hacemos nuestra estrategia
+passport.use(
+  new BasicStrategy(async function(email, password, cb) {
+    try {
+      // optenemos los datos de axios
+      const { data, status } = await axios({
+        url: `${config.apiUrl}/api/auth/sign-in`,
+        method: 'post',
+        auth: {
+          password,
+          username: email
+        },
+        data: {
+          apiKeyToken: config.apiKeyToken
+        }
+      });
+
+      // si no hay data, o el status no es diferente a 200
+      if (!data || status !== 200) {
+        // el usuario es false
+        return cb(boom.unauthorized(), false);
+      }
+
+      //  si hay data, y el estatus es 200
+      return cb(null, data);
+    } catch (error) {
+      cb(error);
+    }
+  })
+);
